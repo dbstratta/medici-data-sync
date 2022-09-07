cc320a50b72ce007af37ff14c28a32ff03f7ac0d
Diego Stratta <18295954+dbstratta@users.noreply.github.com>
2022-09-07 01:27:51 -0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
 use std::path::PathBuf;
 
+use anyhow::Result;
 use clap::{Parser, Subcommand};
 
 mod format;
 mod sync;
 
-#[derive(Parser)]
+#[derive(Parser, Clone, Debug)]
 struct Synchronizer {
     #[clap(subcommand)]
     command: Command,
 }
 
-#[derive(Subcommand)]
+impl Synchronizer {
+    fn run() -> Result<()> {
+        let synchronizer = Synchronizer::parse();
+
+        synchronizer.run_command()
+    }
+
+    fn run_command(self) -> Result<()> {
+        match self.command {
+            Command::Sync { data_path } => {
+                sync::sync(data_path)?;
+            }
+            Command::Format { data_path } => {
+                format::format(data_path)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Subcommand, Clone, Debug)]
 enum Command {
     Format {
         #[clap(short, long, value_parser, value_name = "PATH")]
@@ -24,14 +46,5 @@ enum Command {
 }
 
 fn main() {
-    let synchronizer = Synchronizer::parse();
-
-    match synchronizer.command {
-        Command::Sync { data_path } => {
-            sync::sync(data_path).unwrap();
-        }
-        Command::Format { data_path } => {
-            format::format(data_path).unwrap();
-        }
-    }
+    Synchronizer::run().unwrap();
 }
